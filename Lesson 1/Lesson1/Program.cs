e8a77a89fae1103e2fee7c35ca5db2fea0aa2453
Max <unknown.kaihatsu-sha@yandex.ru>
2021-03-29 22:14:57 +0500

    Lesson 1

---
+++ Lesson 1/Lesson1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите Ваше имя: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine($"Привет, {userName}, сегодня {DateTime.Today}");//ToString("d") - вывод только даты
+
+            Console.ReadKey();
+        }
+    }
+}
