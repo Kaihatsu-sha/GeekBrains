d4c9fde4140801e053ce4566cf0755780dfaa042
Max <unknown.kaihatsu-sha@yandex.ru>
2021-07-14 19:50:00 +0500

    Задание к уроку 6. Графы

---
+++ Introduction to C Sharp/Lesson6/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson6
+{
+    public class Node
+    {
+        public string Value { get; set; }
+        public List<Edge> Edges { get; set; }
+
+        public Node(string value)
+        {
+            Value = value;
+            Edges = new List<Edge>();
+        }
+    }
+}
