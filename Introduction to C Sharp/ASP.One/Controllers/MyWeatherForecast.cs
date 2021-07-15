073184d26a7f7b33b26852a863871e645632445f
Max <unknown.kaihatsu-sha@yandex.ru>
2021-07-15 16:07:08 +0500

    Задание к уроку 1

---
+++ Introduction to C Sharp/ASP.One/Controllers/MyWeatherForecast.cs
@@ -0,0 +1,128 @@
+﻿using ASP.One.Mock;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.One.Controllers
+{
+    [Route("apiv2/wf")]
+    [ApiController]
+    public class MyWeatherForecast : ControllerBase
+    {
+        ILogger<MyWeatherForecast> _logger;
+        IMock _weatherForecastMock;
+
+        public MyWeatherForecast(ILogger<MyWeatherForecast> logger, IMock mock)
+        {
+            _logger = logger;
+            _weatherForecastMock = mock;
+        }
+
+        [HttpPost("show")]
+        public IActionResult ReturnData()
+        {
+            string hourS = HttpContext.Request.Query["hourStart"].ToString();
+            Int32.TryParse(hourS, out int hourStart);
+            string hourE = HttpContext.Request.Query["hourEnd"].ToString();
+            Int32.TryParse(hourE, out int hourEnd);
+
+            if (hourStart == 0 || hourEnd == 0)
+            {
+                return base.Ok(_weatherForecastMock.Get());
+            }
+
+            List<SimpleWeatherForecast> iEnumerable = _weatherForecastMock.Get().ToList();
+            List<SimpleWeatherForecast> show = new List<SimpleWeatherForecast>();
+
+            foreach (SimpleWeatherForecast forecast in iEnumerable)
+            {
+                if (forecast.Time.Hour >= hourStart && forecast.Time.Hour <= hourEnd)
+                {
+                    show.Add(forecast);
+                }
+            }
+            return Ok(show);
+        }
+
+        [HttpPost("del")]
+        public IActionResult DeletedData()
+        {
+            string hourS = HttpContext.Request.Query["hourStart"].ToString();
+            Int32.TryParse(hourS, out int hourStart);
+            string hourE = HttpContext.Request.Query["hourEnd"].ToString();
+            Int32.TryParse(hourE, out int hourEnd);
+
+            if (hourStart == 0 || hourEnd == 0)
+            {
+                return base.Ok(_weatherForecastMock.Get());
+            }
+
+            List<SimpleWeatherForecast> iEnumerable = _weatherForecastMock.Get().ToList();
+            List<SimpleWeatherForecast> delete = new List<SimpleWeatherForecast>();
+
+            foreach (SimpleWeatherForecast forecast in iEnumerable)
+            {
+                if (forecast.Time.Hour >= hourStart && forecast.Time.Hour <= hourEnd)
+                {
+                    delete.Add(forecast);
+                }
+            }
+
+            foreach (SimpleWeatherForecast forecast in delete)
+            {
+                iEnumerable.Remove(forecast);
+            }
+
+            _weatherForecastMock.Set(iEnumerable);
+            return Ok(iEnumerable);
+        }
+
+        [HttpPost("edit")]
+        public IActionResult Edit()
+        {
+            string hourS = HttpContext.Request.Query["hour"].ToString();
+            Int32.TryParse(hourS, out int hour);
+            string temperatureS = HttpContext.Request.Query["temperature"].ToString();
+            Int32.TryParse(temperatureS, out int temperature);
+
+            List<SimpleWeatherForecast> iEnumerable = _weatherForecastMock.Get().ToList();
+
+            foreach (SimpleWeatherForecast forecast in iEnumerable)
+            {
+                if (forecast.Time.Hour == hour)
+                {
+                    forecast.TemperatureC = temperature;
+                    forecast.Summary = "Edited";
+                    return Ok("Отредактирован");
+                }
+            }
+
+            return Ok("Не найдено");
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add()
+        {
+            string hourS = HttpContext.Request.Query["hour"].ToString();
+            Int32.TryParse(hourS, out int hour);
+            string temperatureS = HttpContext.Request.Query["temperature"].ToString();
+            Int32.TryParse(hourS, out int temperature);
+
+            List<SimpleWeatherForecast> iEnumerable = _weatherForecastMock.Get().ToList();
+            SimpleWeatherForecast newItem = new SimpleWeatherForecast()
+            {
+                Time = new TimeOnly(hour, 0),
+                Summary = "Add item",
+                Date = DateTime.Now,
+                TemperatureC = temperature
+            };
+            iEnumerable.Add(newItem);
+            _weatherForecastMock.Set(iEnumerable);
+
+            return Ok("Добавлен объект");
+        }
+    }
+}
---
+++ Introduction to C Sharp/ASP.One/Mock/WeatherForecast.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.One.Mock
+{
+    public class SimpleWeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public TimeOnly Time { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+
+    public interface IMock
+    {
+        public IEnumerable<SimpleWeatherForecast> Get();
+        public void Set(IEnumerable<SimpleWeatherForecast> value);
+    }
+
+    public class WeatherForecastMock : IMock
+    {
+        IEnumerable<SimpleWeatherForecast> _weatherForecasts;
+
+        public WeatherForecastMock()
+        {
+            Random rng = new Random();
+
+            _weatherForecasts = Enumerable.Range(1, 5).Select(index => new SimpleWeatherForecast
+            {
+                Date = DateTime.Now,
+                Time = new TimeOnly(10 + index, 00),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = "Test value"
+            })
+            .ToArray();
+        }
+
+        public IEnumerable<SimpleWeatherForecast> Get()
+        {
+            return _weatherForecasts;
+        }
+
+        public void Set(IEnumerable<SimpleWeatherForecast> weatherForecasts)
+        {
+            _weatherForecasts = weatherForecasts;
+        }
+    }
+}
