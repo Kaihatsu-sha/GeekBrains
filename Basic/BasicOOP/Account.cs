5c779638e236a0e644cd53be5d51ca0191767b38
Max <unknown.kaihatsu-sha@yandex.ru>
2021-10-12 14:18:40 +0500

    Пункт 1

---
+++ Basic/BasicOOP/Account.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicOOP
+{
+    
+    public class Account
+    {
+        ///
+        /// Создать класс счет в банке с закрытыми полями: номер счета, баланс, тип банковского счета (использовать перечислимый тип). 
+        /// Предусмотреть методы для доступа к данным – заполнения и чтения. Создать объект класса, заполнить его поля и вывести информацию об объекте класса на печать
+        ///
+        int _accountNumber;
+        double _accountBalance;
+        AccountTypeEnum _accountType;
+
+        public void SetAccountNumber(int accountNumber)
+        {
+            _accountNumber = accountNumber;   
+        }
+
+        public int GetAccountNumber()
+        {
+            return _accountNumber;
+        }
+
+        public void SetAccountBalance(double accountBalance)
+        {
+            _accountBalance = accountBalance;
+        }
+
+        public double GetAccountBalance()
+        {
+            return _accountBalance;
+        }
+
+        public void SetAccountType(AccountTypeEnum accountType)
+        {
+            _accountType = accountType;
+        }
+
+        public AccountTypeEnum GetAccountType()
+        {
+            return _accountType;
+        }
+    }
+}
---
+++ Basic/BasicOOP/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BasicOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account= new Account();
+            account.SetAccountNumber(1);
+            account.SetAccountBalance(100);
+            account.SetAccountType(AccountTypeEnum.Debit);
+
+            Console.WriteLine($"Номер счета:{account.GetAccountNumber()}\nТип счета:{account.GetAccountType()}\nБаланс:{account.GetAccountBalance()}");
+            Console.Read();
+        }
+    }
+}
