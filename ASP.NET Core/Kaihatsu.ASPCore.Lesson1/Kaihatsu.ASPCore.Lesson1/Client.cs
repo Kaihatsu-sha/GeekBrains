1adf94e68ac1f386062e0a3207262cefc659298a
Kaihatsu <unknown.kaihatsu-sha@yandex.ru>
2022-02-07 20:32:22 +0500

    Задание 1

---
+++ ASP.NET Core/Kaihatsu.ASPCore.Lesson1/Kaihatsu.ASPCore.Lesson1/Client.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Json;
+
+namespace Kaihatsu.ASPCore.Lesson1
+{
+    internal class Client
+    {
+        private HttpClient _httpClient;
+        public Client()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<Post> GetPost(string url)
+        {
+            Console.WriteLine($"Start getting { url}");
+
+            Task<HttpResponseMessage> taskResponce = _httpClient.GetAsync(url);
+            HttpResponseMessage responce = await taskResponce;
+            
+            responce.EnsureSuccessStatusCode();
+
+            LongOperation(url);
+            Console.WriteLine($"Complit getting { url}");
+            var post = await responce.Content.ReadFromJsonAsync<Post>();
+
+            Console.WriteLine($"Complit transform { url}");
+            return post;
+        }
+
+        private void LongOperation(string url) 
+        {
+            Random random = new Random();
+            int delayTime = random.Next(1000, 10000);
+            Console.WriteLine($"Delay: { delayTime} for {url}");
+            Task.Delay(delayTime);
+        }
+    }
+}
---
+++ ASP.NET Core/Kaihatsu.ASPCore.Lesson1/Kaihatsu.ASPCore.Lesson1/Writer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaihatsu.ASPCore.Lesson1
+{
+    internal class Writer
+    {
+        private string _path;
+        public Writer(string path)
+        {
+            _path = path;
+        }
+
+        public async Task AddInFile(Post post)
+        {
+            using (FileStream streamWriter = new FileStream(_path, FileMode.Append))
+            {
+                try
+                {
+
+                    string postStr = post.ToString();
+                    byte[] buffer = new byte[postStr.Length];
+                    buffer = Encoding.ASCII.GetBytes(postStr);
+                    await streamWriter.WriteAsync(buffer);
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+        }
+    }
+}
