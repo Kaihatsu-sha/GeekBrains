536049528fa11eb2905d0810b3ddaf644b6851be
Kaihatsu <unknown.kaihatsu-sha@yandex.ru>
2022-02-22 19:18:13 +0500

    Добавлена валидация

--- ASP.NET Core/Kaihatsu.Timesheets/Kaihatsu.Timesheets.WebAPI.Data/Employee.cs
+++ ASP.NET Core/Kaihatsu.Timesheets/Kaihatsu.Timesheets.WebAPI.Data/Employee.cs
@@ -1,15 +1,12 @@
 ﻿using Kaihatsu.Timesheets.Core.Abstraction.Data;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Kaihatsu.Timesheets.WebAPI.Data
 {
     public class Employee : ItemBase
     {
+        [Required]
+        [Range(1,long.MaxValue)]
         public long UserId { get;set; }
         public virtual User User { get;set; }
     }
