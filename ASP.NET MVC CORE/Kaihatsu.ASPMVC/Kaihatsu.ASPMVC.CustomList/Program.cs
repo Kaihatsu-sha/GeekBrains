de8cb3579d5620aa1cb87a20fe08d1e6965a4703
Kaihatsu <unknown.kaihatsu-sha@yandex.ru>
2022-03-29 21:45:27 +0500

    Fix

---
+++ ASP.NET MVC CORE/Kaihatsu.ASPMVC/Kaihatsu.ASPMVC.CustomList/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Kaihatsu.ASPMVC.CustomList;
+
+CustomConcurrentList<int> concurentList = new CustomConcurrentList<int>();
+
+concurentList.Contains(1);
+concurentList.Add(1);
+concurentList.Add(2);
+concurentList.Contains(1);
+concurentList.Add(3);
+concurentList.Add(4);
+concurentList.Add(5);
+concurentList.Remove(6);
+concurentList.Remove(1);
+concurentList.Add(7);
+
+concurentList.Clear();
+concurentList.Add(1);
+concurentList.Add(2);
+concurentList.Insert(1, 8);
+
+concurentList.IndexOf(5);
+
+foreach (int item in concurentList)
+{
+    Console.WriteLine(item);
+}
