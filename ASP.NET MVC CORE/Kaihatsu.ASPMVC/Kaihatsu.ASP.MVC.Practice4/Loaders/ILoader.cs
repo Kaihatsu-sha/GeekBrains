8b8827c56d47d2608eb0b1df2628d5afee95a264
Kaihatsu <unknown.kaihatsu-sha@yandex.ru>
2022-04-11 12:53:10 +0500

    Практика к уроку 4

---
+++ ASP.NET MVC CORE/Kaihatsu.ASPMVC/Kaihatsu.ASP.MVC.Practice4/Loaders/ILoader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaihatsu.ASP.MVC.Practice4.Loaders;
+
+internal interface ILoader
+{
+    public string LoadFrom(string path);
+}
---
+++ ASP.NET MVC CORE/Kaihatsu.ASPMVC/Kaihatsu.ASP.MVC.Practice4/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Kaihatsu.ASP.MVC.Practice4;
+using Kaihatsu.ASP.MVC.Practice4.Models;
+using Kaihatsu.ASP.MVC.Practice4.Serializers;
+
+
+Console.WriteLine("Задание к лекции 4:" + Environment.NewLine);
+
+Facade facade = new Facade();
+
+string pathToMonbento = @"C:\Users\User\source\GeekBrains\ASP.NET MVC CORE\Kaihatsu.ASPMVC\Kaihatsu.ASP.MVC.Practice4\bin\Debug\net6.0\monbento_me.json";
+string pathTastyCoffee = @"C:\Users\User\source\GeekBrains\ASP.NET MVC CORE\Kaihatsu.ASPMVC\Kaihatsu.ASP.MVC.Practice4\bin\Debug\net6.0\shop_tastycoffee_ru.json";
+
+List<Product> products = facade.GetProductFrom(pathToMonbento, pathTastyCoffee);
+
+
+foreach (Product product in products)
+{
+    Console.WriteLine(product.ToString() + Environment.NewLine);
+}
+
+Console.WriteLine("Конец. Нажмите любую клафишу для завершения");
+
+Console.ReadLine();
