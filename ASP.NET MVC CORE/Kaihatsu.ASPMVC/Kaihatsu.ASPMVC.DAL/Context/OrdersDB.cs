e623d452b776853d9cd3aaf6bb33c604efbaaa93
Kaihatsu <unknown.kaihatsu-sha@yandex.ru>
2022-04-16 17:10:13 +0500

    Adding DAL

---
+++ ASP.NET MVC CORE/Kaihatsu.ASPMVC/Kaihatsu.ASPMVC.DAL/Context/OrdersDB.cs
@@ -0,0 +1,18 @@
+﻿
+using Kaihatsu.ASPMVC.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kaihatsu.ASPMVC.DAL.Context;
+
+public class OrdersDB : DbContext
+{
+    public DbSet<Buyer> Buyers { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Order> Orders { get; set; }
+
+
+    public OrdersDB(DbContextOptions<OrdersDB> options):base(options)
+    {
+
+    }
+}
---
+++ ASP.NET MVC CORE/Kaihatsu.ASPMVC/Kaihatsu.ASPMVC.DAL/Entities/Product.cs
@@ -0,0 +1,12 @@
+﻿using Kaihatsu.ASPMVC.DAL.Entities.Base;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Kaihatsu.ASPMVC.DAL.Entities;
+
+public class Product : Entity
+{
+    public string Name { get; set; }
+    [Column(TypeName="decimal(18,2)")]
+    public decimal Price { get; set; }
+    public string? Category { get; set; }
+}
